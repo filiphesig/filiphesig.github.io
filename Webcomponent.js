9b398a67e59f1e8154a6ba3bc1239dcd8910d1e0
filiphesig <164007736+filiphesig@users.noreply.github.com>
2024-03-19 13:07:41 +0100

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -21,7 +21,14 @@
         }
 
         fireChanged() {
-            console.log("OnClick Triggered");     
+            console.log("OnClick Triggered");
+			let url = 'https://example.com';
+
+			fetch(url)
+			.then(res => res.json())
+			.then(out =>
+				console.log('Checkout this JSON! ', out))
+			.catch(err => { throw err });
             
         }        
         
